aa7d272b28b9b9ac1f7f1aa07306ecf9fc4c489c
dangzed <dangyeuzed@gmail.com>
2020-05-04 16:31:58 +0700

    delete transfer route

--- 1-demo/index.js
+++ 1-demo/index.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser')
-const csurf = require('csurf')
 
 const userRoute = require('./routes/user.route')
 const authRoute = require('./routes/auth.route')
 const authMiddleware = require('./middlewares/auth.middleware')
-const transferRoute = require('./routes/transfer.route')
 
 const app = express();
 
@@ -18,11 +16,9 @@ app.use(cookieParser('best zed in the world'))
 
 app.use('/users', authMiddleware.requireAuth, userRoute)
 app.use('/auth', authRoute)
-app.use('/transfer', authMiddleware.requireAuth, transferRoute)
 app.get('/', (req, res) => {
     res.render('home')
 })
-app.use(csurf({cookie: true}))
 // listen for requests
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
--- 1-demo/routes/transfer.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const controller = require('../controller/transfer.controller')
-const router = express.Router()
-
-router.get('/create', controller.getCreate)
-router.post('/create',controller.postCreate)
-
-module.exports = router
